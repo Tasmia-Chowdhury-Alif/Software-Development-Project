7ce78d86f26185f94a9ab9f3112454e0238e7724
Tasmia Chowdhury Alif <tasmiachowdhuryalif222@gmail.com>
2025-03-19 12:53:52 +0600

    Project 5 learned form usig POST and Get request

---
+++ Module - 12 ( Forms in Django )/fifth_project/first_app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'first_app/home.html')
+
+def about(request):
+    if request.method == 'POST':
+        print(request.POST)
+        name = request.POST.get('username')
+        email = request.POST.get('email')
+        select = request.POST.get('select')
+        return render(request, 'first_app/about.html', context= {'name' : name, 'email' : email, 'select' : select, })
+    
+    return render(request, 'first_app/about.html')
+
+def submit_form(request):
+    
+    return render(request, 'first_app/form.html')
