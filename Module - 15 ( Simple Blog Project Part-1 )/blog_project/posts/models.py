09fc94731951b50a559f81c395c3d7826e7ee72e
Tasmia Chowdhury Alif <tasmiachowdhuryalif222@gmail.com>
2025-04-20 22:37:02 +0600

    Created model for author, categories, posts and profiles also migated them . besides created a super user with username'admin' and pass'123'

--- Module - 15 ( Simple Blog Project Part-1 )/blog_project/posts/models.py
+++ Module - 15 ( Simple Blog Project Part-1 )/blog_project/posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from categories.models import Category
+from author.models import Author
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    category = models.ManyToManyField(Category) # Many to Many relationship because = one post can have multiple category and also one category can contain multiple posts
+    author = models.ForeignKey(Author, on_delete=models.CASCADE) # Many to one relationship because = Multiple posts can have same author and also one author can have multiple posts
+    
+    def __str__(self):
+        return self.title
