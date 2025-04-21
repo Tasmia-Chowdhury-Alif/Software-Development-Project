30576f26ea927c2fafdedeaff38459998d0fb609
Tasmia Chowdhury Alif <tasmiachowdhuryalif222@gmail.com>
2025-04-21 22:33:09 +0600

    Blog project part-1  comlpleted

--- Module - 15 ( Simple Blog Project Part-1 )/blog_project/posts/urls.py
+++ Module - 15 ( Simple Blog Project Part-1 )/blog_project/posts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('add/', views.add_post, name= 'AddPost')
+    path('add/', views.add_post, name= 'AddPost'),
+    path('edit/<int:id>', views.edit_post, name= 'EditPost'),
+    path('delete/<int:id>', views.delete_post, name= 'DeletePost'),
 ]
