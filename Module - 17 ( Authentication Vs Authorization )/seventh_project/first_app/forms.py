f542436bbaff75af6e96bd6d76d8a8ee9bc25db2
Tasmia Chowdhury Alif <tasmiachowdhuryalif222@gmail.com>
2025-05-11 21:56:36 +0600

    Signup form is ready

--- Module - 17 ( Authentication Vs Authorization )/seventh_project/first_app/forms.py
+++ Module - 17 ( Authentication Vs Authorization )/seventh_project/first_app/forms.py
@@ -3,8 +3,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms 
 
 class RegistrationForm(UserCreationForm):
-    first_name = forms.CharField(widget=forms.TextInput(attrs={'id' : 'required'}))
-    last_name = forms.CharField(widget=forms.TextInput(attrs={'id' : 'required'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'required': ''}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'required': ''}))
+    email = forms.EmailField(required=True, widget=forms.EmailInput())
 
     class Meta:
         model = User
--- Module - 17 ( Authentication Vs Authorization )/seventh_project/first_app/views.py
+++ Module - 17 ( Authentication Vs Authorization )/seventh_project/first_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -8,6 +9,10 @@ def home(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
+            # print(form.cleaned_data)
+            messages.success(request, 'Account Created Successfully')
+            # messages.warning(request, 'Warning message')
+            # messages.info(request, 'Info message')
             return redirect('homepage')
 
     return render(request, 'home.html', context={'form' : form})
