23be713765d683a78651295c4ca95095207222f7
Tasmia Chowdhury Alif <tasmiachowdhuryalif222@gmail.com>
2025-05-17 15:32:40 +0600

    Create Update and Delete Class Based View implemented

--- Module - 19 ( Class Based View and Blog Project Part-3)/blog_project/posts/urls.py
+++ Module - 19 ( Class Based View and Blog Project Part-3)/blog_project/posts/urls.py
@@ -2,7 +2,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('add/', views.add_post, name= 'AddPost'),
-    path('edit/<int:id>', views.edit_post, name= 'EditPost'),
-    path('delete/<int:id>', views.delete_post, name= 'DeletePost'),
+    # path('add/', views.add_post, name= 'AddPost'),
+    path('add/', views.AddPostView.as_view(), name= 'AddPost'),
+    # path('edit/<int:id>', views.edit_post, name= 'EditPost'),
+    path('edit/<int:id>', views.EditPostView.as_view(), name= 'EditPost'),
+    # path('delete/<int:id>', views.delete_post, name= 'DeletePost'),
+    path('delete/<int:id>', views.DeletePostView.as_view(), name= 'DeletePost'),
 ]
--- Module - 19 ( Class Based View and Blog Project Part-3)/blog_project/posts/views.py
+++ Module - 19 ( Class Based View and Blog Project Part-3)/blog_project/posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from . import forms, models
 from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView, UpdateView, DeleteView
 
 # Create your views here.
 
@@ -17,6 +19,19 @@ def add_post(request):
     
     return render(request, 'posts/add_post.html', context={'form' : post_form}) # according to best practice django app's templates must contain a folder named as same as the app name. so the template path is posts/add_post.html
 
+# add post using class based view
+class AddPostView(CreateView):
+    model = models.Post
+    form_class = forms.PostForm 
+    template_name = 'posts/add_post.html'
+    success_url = reverse_lazy('HomePage')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+
 @login_required
 def edit_post(request, id):
     post = models.Post.objects.get(pk= id) # selecting the specific Post object using get method and primarikey
@@ -31,8 +46,24 @@ def edit_post(request, id):
     
     return render(request, 'posts/add_post.html', context={'form' : post_form}) # according to best practice django app's templates must contain a folder named as same as the app name. so the template path is posts/add_post.html
 
+
+class EditPostView(UpdateView):
+    model = models.Post
+    form_class = forms.PostForm
+    template_name = 'posts/add_post.html'
+    pk_url_kwarg = 'id'
+    success_url = reverse_lazy('HomePage')
+
+
 @login_required
 def delete_post(request, id):
     post = models.Post.objects.get(pk=id)
     post.delete()
     return redirect('HomePage')
+
+
+class DeletePostView(DeleteView):
+    model = models.Post
+    template_name = 'Author/delete.html'
+    pk_url_kwarg = 'id'
+    success_url = reverse_lazy('HomePage')
