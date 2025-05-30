587cdc230e8ec6ac9bdb6ad21fba61c6a58ca16c
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-05-31 00:17:50 +0600

    Signup, Login, LogOut Implemented

---
+++ Module - 20 (Mid Exam  Car Sell Website)/Car_Saler/accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+    path('signup/', views.UserSignupView.as_view(), name='SignupPage'),
+    path('login/', views.UserLoginView.as_view(), name='LoginPage'),
+    path('logout/', views.UserLogoutView.as_view(), name='LogoutPage'),
+]
---
+++ Module - 20 (Mid Exam  Car Sell Website)/Car_Saler/accounts/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
+from .forms import SignupForm
+from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.contrib import messages
+
+
+# Create your views here.
+class UserSignupView(CreateView):
+    form_class = SignupForm
+    template_name = 'accounts/auth_form.html'
+    success_url = reverse_lazy('LoginPage')
+
+    def form_valid(self, form):
+        messages.success(self.request, f'Account { form.cleaned_data['username'] } is Created Successfully. Please LogIn!')
+        return super().form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['type'] = 'SignUp'
+        return context
+        
+
+class UserLoginView(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'accounts/auth_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('HomePage')
+    
+    def form_valid(self, form):
+        messages.success(self.request, "Logged In Succesfully !")
+        return super().form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['type'] = 'LogIn'
+        return context
+    
+
+class UserLogoutView(LogoutView):
+    def get_success_url(self):
+        messages.success(self.request, "LogOut Successfully !")
+        return reverse_lazy('LoginPage')
+    
