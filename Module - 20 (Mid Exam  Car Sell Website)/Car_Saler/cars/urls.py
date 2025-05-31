61fef11416f1b101aa42189791d6ef5acb941e08
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-05-31 23:53:33 +0600

    car details partially implemented

--- Module - 20 (Mid Exam  Car Sell Website)/Car_Saler/cars/urls.py
+++ Module - 20 (Mid Exam  Car Sell Website)/Car_Saler/cars/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.home, name= 'HomePage'),
+    path('details/<int:pk>/', views.CarDetailsView.as_view(), name= 'DetailsPage'),
 ]
--- Module - 20 (Mid Exam  Car Sell Website)/Car_Saler/cars/views.py
+++ Module - 20 (Mid Exam  Car Sell Website)/Car_Saler/cars/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from . import models
+from django.views.generic import DetailView
 
 # Create your views here.
+class CarDetailsView(DetailView):
+    model = models.Car
+    # pk_url_kwarg = 'pk'
+    template_name = 'cars/details.html'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context["car"] = 
+    #     return context
+    
