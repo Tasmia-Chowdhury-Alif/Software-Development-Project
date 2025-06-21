83a280a32713ca4c1b13775c1c8841006bbddb8b
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-06-21 23:35:27 +0600

    Revert "implimenting doctors details page"
    This reverts commit 2b2648c35961360704d03b3bf239ece5d576d92e.

--- Module - 25 (Hospital Management Project Frontend Part One)/doctorDetails.js
+++
@@ -1,9 +0,0 @@
-const getparams = () => {
-    const param = new URLSearchParams(window.location.search).get("doctorId");
-    fetch(`https://testing-8az5.onrender.com/doctor/list/${param}/`)
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-};
-
-getparams();
