82f5ae77d7431ae17488bd850428f1b3750bdaf1
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-06-28 20:33:08 +0600

    created models for service and contact_us app

--- Module - 29 (Creating All Apps and Models)/smart_care/contact_us/models.py
+++ Module - 29 (Creating All Apps and Models)/smart_care/contact_us/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class ContactUs(models.Model):
+    name = models.CharField(max_length= 40)
+    phone = models.CharField(max_length= 14)
+    problem = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "Contact Us"
+
+    def __str__(self):
+        return self.name
+    
