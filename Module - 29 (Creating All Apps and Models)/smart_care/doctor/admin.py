119d70cb53ae6bef20e6a04ca4739ed8a391bcbb
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-06-29 00:05:15 +0600

    created models for patient and doctor app

--- Module - 29 (Creating All Apps and Models)/smart_care/doctor/admin.py
+++ Module - 29 (Creating All Apps and Models)/smart_care/doctor/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Doctor, Designation, Specialization, AvailableTime
 
 # Register your models here.
+class DesignationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+class SpecializationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Doctor)
+admin.site.register(Designation, DesignationAdmin)
+admin.site.register(Specialization, SpecializationAdmin)
+admin.site.register(AvailableTime)
--- Module - 29 (Creating All Apps and Models)/smart_care/doctor/models.py
+++ Module - 29 (Creating All Apps and Models)/smart_care/doctor/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Designation(models.Model):
+    name = models.CharField(max_length= 30)
+    slug = models.SlugField(max_length= 40)
+
+    def __str__(self):
+        return self.name 
+    
+class Specialization(models.Model):
+    name = models.CharField(max_length= 30)
+    slug = models.SlugField(unique=True, max_length= 40)
+
+    def __str__(self):
+        return self.name 
+    
+class AvailableTime(models.Model):
+    time = models.CharField(unique=True, max_length= 100)
+
+    def __str__(self):
+        return self.time
+    
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='doctor')
+    image = models.ImageField(upload_to= "doctor/images/")
+    designation = models.ManyToManyField(Designation)
+    specialization = models.ManyToManyField(Specialization)
+    available_time = models.ManyToManyField(AvailableTime)
+    fee = models.PositiveIntegerField()
+    meet_link = models.CharField(max_length= 200)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
