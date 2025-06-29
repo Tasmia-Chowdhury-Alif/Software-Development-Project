f406f871b9499c0754dcebf5468d424c23cdc6ee
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-06-29 14:05:56 +0600

    initial Project setup

---
+++ Module - 30 (Creating All Serializers)/smart_care/doctor/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Doctor, Designation, Specialization, AvailableTime, Review
+
+# Register your models here.
+class DesignationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+class SpecializationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Doctor)
+admin.site.register(Designation, DesignationAdmin)
+admin.site.register(Specialization, SpecializationAdmin)
+admin.site.register(AvailableTime)
+admin.site.register(Review)
