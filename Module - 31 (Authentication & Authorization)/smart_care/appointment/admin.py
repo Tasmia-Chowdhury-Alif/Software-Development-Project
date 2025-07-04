8a10a06fa6c9f5f460839626df1437de1cb5e2ae
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-07-04 13:02:48 +0600

    appointment email is implemented

--- Module - 31 (Authentication & Authorization)/smart_care/appointment/admin.py
+++ Module - 31 (Authentication & Authorization)/smart_care/appointment/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Appointment
 
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.shortcuts import redirect
+
 # Register your models here.
 class AppointmentAdmin(admin.ModelAdmin):
     list_display = ['doctor_name', 'patient_name', 'appointment_type', 'appointment_status', 'symptom', 'time', 'cancel']
@@ -10,5 +14,15 @@ class AppointmentAdmin(admin.ModelAdmin):
 
     def patient_name(self, obj):
         return obj.patient.user.first_name
+    
+    def save_model(self, request, obj, form, change):
+        if obj.appointment_type == "Online" and obj.appointment_status == "Running" :
+            email_subject = "Yor Online Appointment is Running"
+            email_body = render_to_string('./appointment/appointment_email.html', {"user" : obj.patient.user, "doctor" : obj.doctor})
+
+            email = EmailMultiAlternatives(email_subject, '', to=[obj.patient.user.email])
+            email.attach_alternative(email_body, "text/html")
+            email.send()
+        return super().save_model(request, obj, form, change)
     
 admin.site.register(Appointment, AppointmentAdmin)
